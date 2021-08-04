524683893a6342bb3e1cbced71397ebf65c0238a
hung <hung.le@apps-cyclone.com>
2021-08-04 21:12:31 +0700

    build: init source front end

---
+++ src/components/layout/default/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.scss";
+
+function Footer(props) {
+  return <footer className="footer">Footer</footer>;
+}
+
+export default Footer;
---
+++ src/components/layout/default/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./header.scss";
+
+function Header(props) {
+  return <header className="header">Header</header>;
+}
+
+export default Header;
