475e21221f1fc355617c2e987b20c92438f56040
hung <hung.le@apps-cyclone.com>
2021-08-04 22:25:45 +0700

    build: login page

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -1,8 +1,26 @@
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import React from "react";
 import "./loginPage.scss";
 
 function LoginPage(props) {
-  return <div className="loginPage">login page</div>;
+  return (
+    <div className="loginPage">
+      <form className="form" noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="user" variant="outlined" />
+        <TextField
+          id="standard-password-input"
+          label="Password"
+          type="password"
+          variant="outlined"
+          autoComplete="current-password"
+        />
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
