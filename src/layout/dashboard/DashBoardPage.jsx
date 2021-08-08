d079561f1b14b1681aaa033020cf901ef1e25734
hung <hung.le@apps-cyclone.com>
2021-08-08 20:22:31 +0700

    build: dashboard admmin

--- src/layout/dashboard/DashBoardPage.jsx
+++ src/layout/dashboard/DashBoardPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/header/Header";
 
 function DashBoardPage({ children }) {
   return (
-    <div>
-      dashboard page
+    <div className="adminDashboard">
+      <Header />
       {children}
     </div>
   );
--- src/layout/default/components/header/Header.jsx
+++ src/layout/default/components/header/Header.jsx
@@ -1,27 +1,48 @@
-import Button from "@material-ui/core/Button";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
-import Logo from "../../../../components/common/logo/Logo";
-import "./header.scss";
 
-function Header(props) {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
   return (
-    <header className="header">
-      <Link to="/">
-        <div className="logoContainer">
-          <Logo />
-        </div>
-      </Link>
-
-      <div className="left">
-        <Link to="/login">
-          <Button variant="contained" color="primary">
-            Login
-          </Button>
-        </Link>
-      </div>
-    </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Link to="/login">
+            <Button color="secondary">Login</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
-
-export default Header;
