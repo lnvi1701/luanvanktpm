b79af94f3b7fecdf6589b76b03028a0d9ad4bf52
hung <hung.le@apps-cyclone.com>
2021-08-08 22:49:34 +0700

    build: navigation bar

--- src/layout/dashboard/DashBoard.jsx
+++ src/layout/dashboard/DashBoard.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/header/Header";
+import LeftNavigationBar from "./components/left-navigation-bar/LeftNavigationBar";
+import "./dashBoard.scss";
 
 function DashBoardLayout({ children }) {
   return (
     <div className="adminDashboard">
       <Header />
-      {children}
+      <div className="body">
+        <LeftNavigationBar />
+        {children}
+      </div>
     </div>
   );
 }
---
+++ src/layout/dashboard/components/left-navigation-bar/LeftNavigationBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./leftNavigationBar.scss";
+
+const NAV_ITEM = [
+  { name: "Quản lý vật tư", accessRights: ["user", "admin"] },
+  { name: "Quản lý danh mục vật tư", accessRights: ["user", "admin"] },
+  { name: "Quản lý nhân viên", accessRights: ["admin"] },
+  { name: "Yêu cầu duyệt", accessRights: ["admin"] },
+];
+
+function LeftNavigationBar(props) {
+  const listNav = NAV_ITEM.map((item, index) => {
+    return (
+      <div className="item" key={index}>
+        {item.name}
+      </div>
+    );
+  });
+
+  return (
+    <div className="leftNavigationBar">
+      <div className="wrapper">{listNav}</div>
+    </div>
+  );
+}
+
+export default LeftNavigationBar;
